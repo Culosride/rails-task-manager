029b638ab2b1f39da09507420be6fe54ce37e9f3
Andrea Spiga <drespriz@gmail.com>
2022-05-17 14:41:14 +0200

    added form create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,32 @@
 class TasksController < ApplicationController
+  before_action :select_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_par)
+    # raise
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_par
+    params.require(:task).permit(:title, :details)
+  end
+
+  def select_task
+    @task = Task.find(params[:id])
   end
 end
